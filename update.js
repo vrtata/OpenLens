584511515cdae69df1d8911dc552ee9b0a434a6a
VRamesh <vrtata@gmail.com>
2022-08-13 08:18:54 +0530

    Create update.js

---
+++ update.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+
+var packagejson = require('./package.json');
+
+packagejson.build.publish = {
+    url: "https://github.com/vrtata/OpenLens/releases/download/Latest",
+    provider: "generic"
+};
+
+packagejson.build.win.artifactName = "OpenLens.Setup.${version}.${ext}";
+
+fs.writeFileSync('package.json', JSON.stringify(packagejson));
